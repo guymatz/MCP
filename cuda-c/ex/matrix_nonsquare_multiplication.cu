c20e02905d19e8ffac90b0d33b3353e3048ed311
Guy Matz <guyeric.matz@studenti.unipd.it>
2025-10-31 22:46:01 +0100

    working a little.  why does M[0] + N[0] != P[0]?

--- cuda-c/ex/matrix_nonsquare_multiplication.cu
+++ cuda-c/ex/matrix_nonsquare_multiplication.cu
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// The number of elements is reduced to avoid running out of memory 
+// The number of elements is reduced to avoid running out of memory
 #define M_ROWS 2000	  // Number of rows in the matrix M
 #define M_COLS 3000	  // Number of cols in the matrix M
 #define N_ROWS (M_COLS)  // Number of rows in the matrix N (matching the number of columns of matrix)
@@ -10,24 +10,35 @@ __global__ void matrixAdd(float* M, float* N, float* P, int rows_M, int cols_M, 
 			// d_M,	  d_N,	 d_P,	   M_ROWS,	 M_COLS,	 N_ROWS,	 N_COLS);
 
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	if (idx > M_ROWS * M_COLS) {
-		printf("returning . . .\n");
+	if (idx >= M_ROWS * M_COLS ) {
+		//printf("%i: returning . . .\n", idx);
 		return;
 	}
-	//printf("idx: %i, blockIdx=%i, blockDim=%i, threadIdx=%i\n", idx, blockIdx.x , blockDim.x , threadIdx.x);
-	float p = M[idx] + N[idx];
-	printf("idx: %i, M=%4.2f, N=%4.2f, p=%8.2f\n", idx, M[idx] , N[idx] , p);
-	P[idx] = p;
-
+	float s = M[idx] + N[idx];
+	if (idx < 10) {
+		printf("%i: %4.2f + %4.2f = %4.2f\n", idx, M[idx], N[idx], s);
+	}
+	P[idx] = s;
 	return;
+}
+
+void print_matrix(const char name[1], float* M) {
+	// Print part of the result matrix P for verification
+	printf("%s:\n", name);
+	for (int i = 0; i < 100; i++) {
+		printf("%8.2f", M[i]);
+		if ((i+1) % 10 == 0)
+			printf("\n");
+	}
+	printf("\n");
 }
 
 int main() {
 	// Size in bytes for the ROWS x COLS matrix
-	int matrix_size = M_ROWS * M_COLS;  
-	int size_M = M_ROWS * M_COLS * sizeof(float);  
-	int size_N = N_ROWS * N_COLS * sizeof(float);  
-	int size_P = M_ROWS * N_COLS * sizeof(float);  
+	int matrix_size = M_ROWS * M_COLS;
+	int size_M = matrix_size * sizeof(float);
+	int size_N = matrix_size * sizeof(float);
+	int size_P = matrix_size * sizeof(float);
 
 	// Host memory allocation
 	float *h_M = (float*)malloc(size_M);
@@ -38,10 +49,13 @@ int main() {
 	for (int i = 0; i < M_ROWS * M_COLS; i++) {
 		h_M[i] = 1.0 + (float)rand()/RAND_MAX;
 	}
+	print_matrix("M", h_M);
+
 	// Initialize matrix N
 	for (int i = 0; i < N_ROWS * N_COLS; i++) {
 		h_N[i] = 1.0 + (float)rand()/RAND_MAX;
 	}
+	print_matrix("N", h_N);
 
 	// Device memory allocation
 	float* d_M;
@@ -67,16 +81,10 @@ int main() {
 	matrixAdd<<<N_b, NUM_THREADS>>>(d_M, d_N, d_P, M_ROWS, M_COLS, N_ROWS, N_COLS);
 
 	// Copy the result matrix P from device to host
-	cudaMemcpy(h_P, d_P, matrix_size, cudaMemcpyDeviceToHost);
-	cudaDeviceSynchronize();
+	cudaMemcpy(h_P, d_P, size_M, cudaMemcpyDeviceToHost);
 
 	// Print part of the result matrix P for verification
-	printf("P:\n");
-	for (int i = 0; i < 100; i++) {
-		printf("%8.2f", h_P[i]);
-		if ((i+1) % 10 == 0)
-			printf("\n");
-	}
+	print_matrix("P", h_P);
 
 	// Free device memory
 	cudaFree(d_M);
