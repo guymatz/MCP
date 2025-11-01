a9ec2ddc6c48bd7aa6eb2aaf17a6373133d4b865
Guy Matz <guyeric.matz@studenti.unipd.it>
2025-11-01 20:21:00 +0100

    run the script to get instantaneous compilations upon save

---
+++ cuda-c/ex/watcher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Setting up watches.
+# Watches established.
+# ./ MODIFY matrix_addition.cu
+# ./ MODIFY .matrix_addition.cu.swp
+
+while true; do
+	## ./ CREATE matrix_addition.cu
+	inotifywait -m . -e modify | while read -r d a f; do
+		if echo $f | grep -q ".cu$" ; then
+			bin=${f%%.*}
+			echo $a $f $bin
+			make && ./$bin
+		fi
+	done
+done
