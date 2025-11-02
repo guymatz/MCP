cf7bf6847ed8981bbeb350691c658ba4b8be7e7f
Guy Matz <guyeric.matz@studenti.unipd.it>
2025-11-02 21:55:34 +0100

    added watcher to auto-recompile when a file changes

--- cuda-c/ex/watcher.sh
+++ cuda-c/ex/watcher.sh
@@ -7,10 +7,10 @@
 
 while true; do
 	## ./ CREATE matrix_addition.cu
-	inotifywait -m . -e close_write | while read -r d a f; do
-		if echo $f | grep -q ".cu$" ; then
-			bin=${f%%.*}
-			echo $a $f $bin
+	inotifywait -q -m . -e close_write | while read -r dir action filename; do
+		if [[ "$filename" =~ .cu$ ]]; then
+			bin=${filename%%.*}
+			echo $dir $action $filename $bin
 			make && ./$bin
 		fi
 	done
