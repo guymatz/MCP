6510264d3860300a6cf5136c284d7191464bf4b8
Guy Matz <gmatz@matz.org>
2025-12-09 15:12:31 +0100

    housekeeping

---
+++ project/parallel/watcher.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Setting up watches.
+# Watches established.
+# ./ MODIFY matrix_addition.cu
+# ./ MODIFY .matrix_addition.cu.swp
+
+if [[ "$(uname -a)" =~ Darwin ]]; then
+	cmd="fswatch . -1"
+else
+	cmd="inotifywait -q -m . -e close_write"
+fi
+
+while true; do
+	$cmd | while read filename; do
+		clear
+		date
+		echo "*********** $filename"
+		make clean && make $(basename ${filename%%.*}).x && ./$(basename ${filename%%.*}).x
+	done
+	sleep 1
+done
