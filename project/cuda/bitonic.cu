1d2478eca99799debf3b600c9b74ac603431cabf
Guy Matz <guyeric.matz@studenti.unipd.it>
2026-01-15 23:29:37 +0100

    tidied up

--- project/cuda/bitonic.cu
+++ project/cuda/bitonic.cu
@@ -1,14 +1,8 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <time.h>
-//#include <algorithm>
 #include <vector>
-//#include <assert.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <cmath>
-//#include <benchmark/benchmark.h>
 #include "cudaUtils.cuh"
 
 using namespace std;
@@ -27,18 +21,19 @@ __global__ void sortKernel(int* d_V, int subArraySize, int distance, int N) {
         return;
 
     if ((idx & subArraySize)==0 && d_V[idx] > d_V[swapPartner]) {
+        // swap does not exist in CUDA :-(
         //swap(d_V[idx], d_V[swapPartner]);
         int tmp = d_V[idx];
         d_V[idx] = d_V[swapPartner];
         d_V[swapPartner] = tmp;
     }
     if ((idx & subArraySize)!=0 && d_V[idx] < d_V[swapPartner]) {
+        // swap does not exist in CUDA :-(
         //swap(d_V[swapPartner], d_V[idx]);
         int tmp = d_V[idx];
         d_V[idx] = d_V[swapPartner];
         d_V[swapPartner] = tmp;
     }
-    //printf("%i\t\t%i\t\t%i\t%i\n", subArraySize, distance,  idx, swapPartner);
 }
 
 int main(int argc, char *argv[]) {
@@ -51,9 +46,16 @@ int main(int argc, char *argv[]) {
     std::vector<int> h_V(n);
     populate_vector(h_V, n);
 
+    /*  Why can't I do this?!
+    bool verbose = argv->std::find("-v") != argv.end();
+    //bool verbose = (std::find(argv.begin(), argv.end(), "-v") != argv.end());
+    cout << "verbose: " << verbose << std::endl;
+    */
+
     // Size in bytes for the ROWS x COLS matrix
     int size = n * sizeof(double);
 
+    // Vector before
     for (int i = 0; i < n; i++) {
 //        cout << pp(h_V[i]) << std::endl;
     }
@@ -64,27 +66,22 @@ int main(int argc, char *argv[]) {
     // Device memory allocation
     int mt = getMaxThreads();
     int blocks = ceil( (float)n / mt);
-    //cout << "n: " << n << ", threads: " << mt << ", blocks: " << blocks << std::endl;
     int* d_V;
     cudaMalloc((void **)&d_V, size);
 
-    // Copy matrices A and B from host to device
+    // Copy vector from host to device
     cudaMemcpy(d_V, &(h_V[0]), size, cudaMemcpyHostToDevice);
 
     int distance, subArraySize;
-
-    // Define block and grid sizes
-    //dim3 blockSize(16, 16);
-    //dim3 gridSize((WIDTH + blockSize.x - 1) / blockSize.x, (WIDTH + blockSize.y - 1) / blockSize.y);
-
     // We loop through "sub arrays" of the original vector, 2 elements, then
     // 4 , then 8 . . .
     printf("subArraySize\tdistance\tidx\tswapPartner\n");
     for (subArraySize=2; subArraySize<=n; subArraySize=subArraySize*2 ) {
         // we break the problem into "sub array" halves
         for (distance=subArraySize/2; distance>0; distance=distance/2) {
-            // Launch the matrixTransposition kernel (do NOT copy back the resulting M_T from Device to Host)
+            //cout << "Before: " << subArraySize << " " << distance << std::endl;
             sortKernel<<<blocks, mt>>>(d_V, subArraySize, distance, n);
+            //cout << "After: " << subArraySize << " " << distance << std::endl;
         }
     }
 
@@ -107,7 +104,6 @@ int main(int argc, char *argv[]) {
     }
     clock_gettime(CLOCK_MONOTONIC, &end_time_verify);
     printf("Bitonic Verification time: %.6f seconds\n", get_elapsed_time(start_time_verify, end_time_verify));
-
 
     printf("Bitonic Execution time: %.6f seconds\n", get_elapsed_time(start_time, end_time));
 
