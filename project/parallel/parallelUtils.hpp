9d3f69cbbdfcdcd42ee15068ff23654292f003f4
Guy Matz <guyeric.matz@studenti.unipd.it>
2026-09-02 01:19:28 +0200

    cleaning up parallel

--- project/parallel/parallelUtils.hpp
+++ project/parallel/parallelUtils.hpp
@@ -15,39 +15,29 @@
 using namespace std;
 
 bool verify(std::vector<int> &V, int n, bool verbose = false) {
-  // n is the number of elements in the list
-  // n = 2**N
-  bool verifySucceeded = true;
-  std::vector<int> sortedV(n);
+    // n is the number of elements in the list
+    // n = 2**N
+    bool verifySucceeded = true;
+    std::vector<int> sortedV(n);
 
-  struct timespec start_time, end_time;
-  clock_gettime(CLOCK_MONOTONIC, &start_time);
-  populate_vector(sortedV, n, ".sorted");
-  clock_gettime(CLOCK_MONOTONIC, &end_time);
-  /* printf("Sorted File Load time: %.6f seconds\n",
-   * get_elapsed_time(start_time, end_time)); */
+    struct timespec start_time, end_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+    populate_vector(sortedV, n, ".sorted");
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
 
-  // cout << "Verbose? " << verbose << std::endl;
-  clock_gettime(CLOCK_MONOTONIC, &start_time);
-#pragma omp parallel for
-  for (int i = 0; i < n; i++) {
-    // cout << omp_get_thread_num() << std::endl;
-    if (V[i] != sortedV[i]) {
-      if (verbose) {
-        cout << "n: " << n << std::endl;
-        cout << "i: " << i << std::endl;
-        cout << V[i] << " != " << sortedV[i] << std::endl;
-      }
-      verifySucceeded = false;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        if (V[i] != sortedV[i]) {
+            if (verbose) {
+                cout << "n: " << n << std::endl;
+                cout << "i: " << i << std::endl;
+                cout << V[i] << " != " << sortedV[i] << std::endl;
+            }
+            verifySucceeded = false;
+        }
     }
-  }
-  clock_gettime(CLOCK_MONOTONIC, &end_time);
-  /* printf("Array Comparison time: %.6f seconds\n",
-   * get_elapsed_time(start_time, end_time)); */
-  /*
-  if (verifySucceeded)
-      cout << n << " elements are looking good!" << std::endl;
-  */
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
 
-  return verifySucceeded;
+    return verifySucceeded;
 }
